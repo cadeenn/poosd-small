e096aa37a382e12df86a91e1bbd988dc2a8deaaa
DG-1337 <devgopal745@gmail.com>
2025-02-28 15:13:52 -0500

    Added search functionality to get contacts endpoint

--- LAMPAPI/GetAllContacts.php
+++ LAMPAPI/GetAllContacts.php
@@ -6,28 +6,44 @@
     $data = json_decode(file_get_contents("php://input"), true); 
 
     // Checks to see if required params have been inputted
-    if (!isset($data["ownerID"])) {
-        echo json_encode(["error" => "ownerID is a required field"]); 
+    if (!isset($data["UserId"])) {
+        echo json_encode(["success" => false, "message" => "Did not receive userId to add new contact for user"]); 
         exit; 
     }
 
     // Param for SQL query 
-    $ownerID = $data["ownerID"];
+    $userId = $data['UserId'];     
     
-    // SQL query to fetch data 
-    $sql = "SELECT * FROM Contacts WHERE OwnerID = ?"; 
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $ownerID); 
-    
-    // Executes SQL query and checks if it was valid 
-    if ($stmt->execute()){
-        $res = $stmt->get_result();
-        $contacts = $res->fetch_all(MYSQLI_ASSOC);
-        echo json_encode($contacts); 
-    } else {
-        echo json_encode(["error" => "Could not get contacts for $ownerID"]); 
-        exit; 
+
+    try {
+        // Executes SQL query and checks if it was valid 
+            // SQL query to fetch data 
+            $sql = "SELECT * FROM Contacts WHERE UserId = ?"; 
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("i", $userId); 
+        if ($stmt->execute()){
+            $res = $stmt->get_result();
+            $contacts = $res->fetch_all(MYSQLI_ASSOC);
+            echo json_encode($contacts); 
+        } else {
+            echo json_encode(["success" => false, "message" => "Could not get contacts for $ownerID"]); 
+            exit; 
+        }
+    } catch (Exception $e) {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        echo json_encode(["success" => false, "message" => $e->getMessage()]);
+
     }
+    
+    // // Executes SQL query and checks if it was valid 
+    // if ($stmt->execute()){
+    //     $res = $stmt->get_result();
+    //     $contacts = $res->fetch_all(MYSQLI_ASSOC);
+    //     echo json_encode($contacts); 
+    // } else {
+    //     echo json_encode(["error" => "Could not get contacts for $ownerID"]); 
+    //     exit; 
+    // }
 
     $stmt->close();
     $conn->close(); 
